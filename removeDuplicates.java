1a267783aa95f7e120c608c7c17cc2cef46097fc
Bhushan Patil <bpatil.00001@gmail.com>
2025-09-18 23:49:04 +0530

    Remove Duplicate From a Sorted Array

---
+++ removeDuplicates.java
@@ -0,0 +1,34 @@
+//Leetcode Problem 26 Remaining
+
+import java.util.*;
+
+class Remove{
+    static int Remove(int[] nums){
+        int x = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > nums[x]) {
+                x++;
+                nums[x] = nums[i];
+            }
+        }
+        return x + 1;
+    }
+}
+
+
+class removeDuplicates {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter numbers in an array: ");
+        int n = sc.nextInt();
+
+        int[] num = new int[n];
+        for(int i=0; i<n; i++){
+            System.out.println("Please Enter the Number");
+            num[i] = sc.nextInt();
+        }
+
+        Remove rm = new Remove();
+        int[]  numbers = rm.Remove(num);
+    }
+}
