82c152ca8465c143fcbaab7a07551061b90f1798
Bhushan Patil <bpatil.00001@gmail.com>
2025-09-23 06:25:05 +0530

    Counting Digits in a Number Optimized

--- CountDigit.java
+++ CountDigit.java
@@ -1,14 +1,27 @@
-// Method 1
+// Method 2
+//Strivers Know Basic Maths Problem 1
 import java.util.*;
 
 class count{
     public int countDigit(int n){
-        int count = 0;
-        while(n!=0){
-           n/= 10;
-           ++count;
-        }
-        return count;
+        // Initialize a variable 'cnt' to
+        // store the count of digits.
+        int cnt = (int) (Math.log10(n) + 1);
+
+        // The expression (int)(Math.log10(n) + 1)
+        // calculates the number of digits in 'n'
+        // and casts it to an integer.
+
+        // Adding 1 to the result accounts
+        // for the case when 'n' is a power of 10,
+        // ensuring that the count is correct.
+
+        // Finally, the result is cast
+        // to an integer to ensure it is rounded
+        // down to the nearest whole number.
+
+        // Return the count of digits in 'n'.
+        return cnt;
     }
 }
 
