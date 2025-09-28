954875487a572b7425756fe3c02a699697526e53
Bhushan Patil <bpatil.00001@gmail.com>
2025-09-28 22:49:01 +0530

    PrimeNumber Brute Force Approach Added

---
+++ PrimeNumber.java
@@ -0,0 +1,34 @@
+//Brute Force Approach
+
+import java.util.*;
+
+class PrimeNumber {
+    static boolean checkPrime(int n){
+        int count=0;
+        for(int i=1; i<=n; i++){
+            if(n%i == 0){
+                count += 1;
+            }
+        }
+        if(count == 2){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter the number");
+        int num = sc.nextInt();
+
+        if (checkPrime(num)) {
+            System.out.println(num + " is a Prime Number.");
+        } else {
+            System.out.println(num + " is NOT a Prime Number.");
+        }
+
+        sc.close();
+
+    }
+}
