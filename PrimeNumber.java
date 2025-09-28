ce96078996a96662fb70d669dc82b8a1593a2424
Bhushan Patil <bpatil.00001@gmail.com>
2025-09-28 22:52:44 +0530

    PrimeNumber Optimal Approach added

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,13 +1,16 @@
-//Brute Force Approach
+//Optimal Approach
 
 import java.util.*;
 
 class PrimeNumber {
     static boolean checkPrime(int n){
         int count=0;
-        for(int i=1; i<=n; i++){
+        for(int i=1; i<=Math.sqrt(n); i++){
             if(n%i == 0){
                 count += 1;
+            }
+            if(n / i != i){
+                count = count + 1;
             }
         }
         if(count == 2){
