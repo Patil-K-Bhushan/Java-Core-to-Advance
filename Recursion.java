694ad26573bf12a39172e9eb82c5bfa697bfa0a7
Bhushan Patil <bpatil.00001@gmail.com>
2025-09-29 05:52:16 +0530

    Basic Recursion Program

---
+++ Recursion.java
@@ -0,0 +1,18 @@
+//Basic Recursion Program
+
+class Recursion {
+    static int count = 0;
+
+    static void print(){
+        if(count == 3) return;
+        System.out.println(count);
+
+        count = count + 1;
+        print();
+    }
+
+    public static void main(String[] args) {
+        print();
+    }
+
+}
